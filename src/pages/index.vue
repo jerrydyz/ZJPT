44f42189cbbb2f12532f85f33236396f75dd9b71
joye <1412959347@qq.com>
2019-07-24 11:10:38 +0800

    测试一下

---
+++ src/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="index">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    this.$axios.post("https://www.easy-mock.com/mock/5d303349a6ecd97aa68d2ab7/example/test").then(response => {
+        console.log(response.data.data.name);
+        this.msg=response.data.data.name;
+        
+      })
+      .catch(response => {
+        console.log(response);
+      });
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
