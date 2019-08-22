9fa313b344eb8c927975c699c86cf6411783f5fc
joye <1412959347@qq.com>
2019-08-22 15:18:11 +0800

    lalala

--- src/pages/video.vue
+++ src/pages/video.vue
@@ -144,7 +144,7 @@ export default {
 	        sources: [
 				{
 					type: 'application/x-mpegURL',
-					src: 'http://jixujiaoyu_api.songlongfei.club/hls/576537c51c12c29adb33c072b48dda2f.m3u8'
+					src: ''
 				}
 			],
 	        //poster: " ", //你的封面地址
